8dc5ba72a5595a0cb5b9d851487d5e6020cfd868
sofia-chiodi <chiodi.sb@gmail.com>
2025-08-27 16:34:44 -0300

    Creating Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import { Sparkles, X } from 'lucide-react'
+
+export const Header = () => {
+  return (
+    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration`}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center py-4">
+            <div className=" flex items-center space-x-2">
+              <div className='relative'>
+                <Sparkles strokeWidth={2.1} className={`w-8 h-8 transition-all duration-300 `} />
+              </div>
+            
+              <h1 
+              className={`text-2xl font-medium bg-gradient-to-r from-purple-200 to-pink-300 bg-clip-text transition-all duration-300`}
+              >
+                Sofía Chiodi
+              </h1>
+            </div>
+
+            <nav className='hidden md:\flex space-x-8'>
+
+              <button 
+              className={`capitalize font-medium transition-all duration-300 hover:scale-105 relative group`}
+              >
+                Home
+                <span className='absolute -bottom-1 left-0 w-0 h-0.5 bg-gradient-to-r from-purple-200 to-pink-300 transition-all duration-300 group-hover:w-full '></span>
+              </button>
+            </nav>
+
+            <div className='md:hidden'>
+              
+            </div>
+          </div>
+      </div>
+    </header>
+  )
+}
