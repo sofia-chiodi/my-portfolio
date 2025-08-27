eca7e06389fdb9de2a7bfb238769b2040dee3ff7
sofia-chiodi <chiodi.sb@gmail.com>
2025-08-27 20:06:26 -0300

    Creating Utils file

---
+++ src/components/lib/Utils.js
@@ -0,0 +1,8 @@
+const menuItems = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Projects', path: '/projects' },
+    { label: 'Contact', path: '/contact' },
+];  
+
+export default menuItems;
