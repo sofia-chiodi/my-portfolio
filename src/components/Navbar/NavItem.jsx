f2b2a21bad311ae3724ab2c6c1f957e5b71bfa5b
sofia-chiodi <chiodi.sb@gmail.com>
2025-08-28 19:19:11 -0300

    Creating a separate NavItem component

---
+++ src/components/Navbar/NavItem.jsx
@@ -0,0 +1,16 @@
+export const NavItem = ({ label, isResponsiveMenuOpen }) => {
+  return (
+    <button
+      className={`${
+        isResponsiveMenuOpen
+          ? 'block w-full text-left px-6 py-3 text-gray-600 hover:bg-gradient-to-r hover:from-violet-100 hover:to-pink-200 hover:text-violet-400'
+          : 'capitalize font-medium transition-all duration-300 hover:scale-105 relative group'
+      } capitalize font-medium transition-all duration-150`}
+    >
+      {label}
+      {!isResponsiveMenuOpen && (
+        <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-gradient-to-r from-violet-200 to-pink-300 transition-all duration-300 group-hover:w-full "></span>
+      )}
+    </button>
+  )
+}
