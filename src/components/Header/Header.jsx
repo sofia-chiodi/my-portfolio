0e9577821f07a1937b4189e076716c8abbd0b3c1
sofia-chiodi <chiodi.sb@gmail.com>
2025-08-29 09:29:19 -0300

    Updating Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
+
 import { Logo } from '@/components/Logo/Logo.jsx'
 import { Navbar } from '@/components/Navbar/Navbar.jsx'
+import { ResponsiveNavbar } from '@/components/Navbar/ResponsiveNavbar.jsx'
 
 export const Header = () => {
+  const [isResponsiveMenuOpen, setIsResponsiveMenuOpen] = useState(false)
+
+  const toggleResponsiveMenu = () => {
+    setIsResponsiveMenuOpen((prev) => !prev)
+  }
+
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration`}
@@ -9,8 +19,25 @@ export const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <Logo />
-          <Navbar />
+          <Navbar isResponsiveMenuOpen={isResponsiveMenuOpen} />
+
+          {/* Responsive Menu Button */}
+          <button
+            className={`md:hidden p-2 rounded-lg transition-all duration-300`}
+            onClick={toggleResponsiveMenu}
+          >
+            {isResponsiveMenuOpen ? (
+              <X size={24} strokeWidth={2.1} />
+            ) : (
+              <Menu size={24} strokeWidth={2.1} />
+            )}
+          </button>
         </div>
+
+        {/* Responsive Navbar */}
+        {isResponsiveMenuOpen && (
+          <ResponsiveNavbar isResponsiveMenuOpen={isResponsiveMenuOpen} />
+        )}
       </div>
     </header>
   )
