e796b0c8e9654eae87ce6144c5a970f2ee313176
sofia-chiodi <chiodi.sb@gmail.com>
2025-08-29 21:43:57 -0300

    adding styling

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,17 +1,21 @@
 import { Sparkles } from 'lucide-react'
 
-export const Logo = () => {
+export const Logo = ({ isScrolled }) => {
   return (
     <div className=" flex items-center space-x-2">
       <div className="relative">
         <Sparkles
           strokeWidth={2.1}
-          className={`w-8 h-8 transition-all duration-300 `}
+          className={`w-8 h-8 transition-all duration-300 ${
+            isScrolled ? 'text-violet-400' : 'text-white'
+          }`}
         />
       </div>
 
       <h1
-        className={`text-2xl font-medium bg-gradient-to-r from-violet-200 to-pink-300 bg-clip-text transition-all duration-300`}
+        className={`text-2xl font-medium bg-gradient-to-r from-violet-200 to-pink-300 bg-clip-text transition-all duration-300 ${
+          isScrolled ? 'text-violet-400' : 'text-white'
+        }`}
       >
         Sofía Chiodi
       </h1>
