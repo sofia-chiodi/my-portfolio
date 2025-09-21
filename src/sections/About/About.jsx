4d5f5a44787a3f8f00fc3f3e0b0094c4b8f7510f
sofia-chiodi <chiodi.sb@gmail.com>
2025-09-21 17:56:29 -0300

    Adding .txt styling to About Me description

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -4,42 +4,50 @@ import skillsList from '../../utils/skillsList'
 export const About = () => {
   return (
     <section id='about'>
-      <div className='min-h-screen py-32 bg-gradient-to-r from-gray-50 to-white relative overflow-hidden'>
+      <div className='min-h-screen py-32 relative overflow-hidden'>
         <div className='absolute inset-0 opacity-25'>
           <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-r from-violet-300 via-pink-300 to-pink-600 transform rotate-12 scale-150'></div>
         </div>
 
-        <div className='max-w-7xl mx-auto py-4 px-10 md:px-20 lg:px-10 relative z-10'>
+        <div className='max-w-7xl mx-auto py-4 px-8 md:px-6 lg:px-10 relative z-10'>
           <div className='text-center mb-16'>
-            <div className='inline-flex px-8 py-4 bg-gradient-to-r from-purple-200 to-pink-200 text-3xl rounded-full mb-8 font-bold'>
+            <div className='inline-flex px-8 py-4 bg-gradient-to-r from-purple-200 to-pink-200 text-4xl rounded-full font-bold'>
               <span className='animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-violet-300 to-pink-300'>
-                ✨ About Me ✨
+                <h2 className='leading-tight'>✨ About Me ✨</h2>
               </span>
             </div>
+          </div>
 
-            <p className='mt-6 text-2xl md:text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-300 to-pink-300 mb-6 max-w-3xl mx-auto '>
-              I'm a{' '}
-              <span className='font-bold text-violet-300/80'>front-end</span>{' '}
-              developer from 📍 Buenos Aires, Argentina, who loves creating
-              innovative and intuitive experiences for users.
-            </p>
-
-            <p className='mt-6 text-lg md:text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-violet-300 to-violet-300/50 mb-6 max-w-3xl mx-auto'>
-              I've created this portfolio as a fun way to showcase my projects
-              and some of my skills regarding this{' '}
-              <span className='text-shadow-2xs'>tech stack</span>:
-            </p>
+          {/* Description */}
+          <div className='retro-window mx-auto max-w-3xl mb-16 border border-pink-300/60 shadow-xl'>
+            <div className='window-header flex items-center justify-between px-4 py-2 bg-gradient-to-r from-pink-200 to-pink-200/60 text-md font-mono text-pink-400/70 border-b border-pink-300/60'>
+              <h3 className='font-bold'>about_me.txt</h3>
+              <button className='text-pink-400/70 hover:scale-105 hover:text-violet-400/70 transition-colors'>
+                X
+              </button>
+            </div>
+            <div className='window-body bg-pink-50 text-violet-400/80 p-6 font-mono text-base'>
+              <p>
+                I'm a{' '}
+                <span className='font-bold text-pink-400/50'>front-end</span>{' '}
+                developer from{' '}
+                <span className='bg-pink-400/50 text-transparent bg-clip-text font-bold font-mono'>
+                  📍 Buenos Aires, Argentina
+                </span>{' '}
+                who loves creating innovative and intuitive experiences for
+                users.
+              </p>{' '}
+              <p className='mt-5'>
+                I've created this portfolio as a fun way to showcase some of my
+                projects and skills.
+              </p>
+            </div>
           </div>
 
           {/* Stack */}
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+          <div className='grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-4 lg:gap-6'>
             {skillsList.map((skill, skillIndex) => (
-              <Card
-                key={skillIndex}
-                title={skill.title}
-                description={skill.description}
-                icon={skill.icon}
-              />
+              <Card key={skillIndex} title={skill.title} icon={skill.icon} />
             ))}
           </div>
 
