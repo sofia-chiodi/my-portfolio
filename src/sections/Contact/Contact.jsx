004847afb2d84a37cca1134d2592e0e1c6f908fa
sofia-chiodi <chiodi.sb@gmail.com>
2025-10-01 20:22:52 -0300

    Updating react-leaflet map server for deployment

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -207,8 +207,8 @@ export const Contact = () => {
             className='h-96 w-full rounded-2xl shadow-lg mt-6'
           >
             <TileLayer
-              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
-              url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
             <Marker position={markerPosition} icon={mapIcon}>
               <Popup>
