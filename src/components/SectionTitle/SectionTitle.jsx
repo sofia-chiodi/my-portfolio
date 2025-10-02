947e5bf4358f0bdd697f99de0100f1780fb997d3
sofia-chiodi <chiodi.sb@gmail.com>
2025-10-02 11:23:01 -0300

    Adding shadow to section titles

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,6 +1,6 @@
 export const SectionTitle = ({ children }) => {
   return (
-    <div className='inline-flex px-8 py-4 bg-gradient-to-r from-pink-200 to-purple-200 text-4xl rounded-full mb-16 font-bold'>
+    <div className='inline-flex px-8 py-4 bg-gradient-to-r from-pink-200 to-purple-200 text-4xl rounded-full mb-16 font-bold shadow-xl shadow-pink-900/10'>
       <span className='animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-violet-300'>
         <h2>{children}</h2>
       </span>
