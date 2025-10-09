22f5f219ddcaa8a8c699892d349039fc67b3352f
sofia-chiodi <chiodi.sb@gmail.com>
2025-10-09 12:03:17 -0300

    Adjusting portfolio section

--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -69,9 +69,10 @@ export const Portfolio = () => {
                   />
 
                   <div className='absolute inset-0 bg-gradient-to-t from-black/50 via-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-all duration-200'>
-                    <div className='absolute bottom-4 left-4 right-4 flex justify-between items-end'>
-                      <div className='flex space-x-3'>
-                        <div className='flex items-center space-x-1 bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 hover:scale-110 transition-all duration-100'>
+                    <div className='absolute items-end bottom-4 right-4'>
+                      {/* <div className='absolute bottom-4 left-4 right-4 flex justify-between items-end'> */}
+                      {/* <div className='flex space-x-3'> */}
+                      {/* <div className='flex items-center space-x-1 bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 hover:scale-110 transition-all duration-100'>
                           <a
                             href={project.demoUrl}
                             target='_blank'
@@ -86,9 +87,9 @@ export const Portfolio = () => {
                               Demo
                             </span>
                           </a>
-                        </div>
+                        </div> */}
 
-                        <div className='flex items-center space-x-1 bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 hover:scale-110 transition-all duration-300'>
+                      {/* <div className='flex items-center space-x-1 bg-white/20 backdrop-blur-sm rounded-full px-4 py-2 hover:scale-110 transition-all duration-300'>
                           <a
                             href={project.githubUrl}
                             target='_blank'
@@ -101,9 +102,9 @@ export const Portfolio = () => {
                             </span>
                           </a>
                         </div>
-                      </div>
+                      </div> */}
 
-                      <button className='w-10 h-10 bg-gray-100/30 backdrop-blur-sm rounded-full flex items-center justify-center hover:scale-110 transition-all duration-300'>
+                      <button className='w-10 h-10 bg-violet-300/30 backdrop-blur-sm rounded-full flex items-center justify-center hover:scale-110 transition-all duration-300'>
                         <a
                           href={project.githubUrl}
                           target='_blank'
--- src/utils/projectFilters.js
+++ src/utils/projectFilters.js
@@ -1,7 +1,6 @@
 const projectFilters = [
   { id: 'all', label: 'All' },
   { id: 'web', label: 'Web' },
-  { id: 'mobile', label: 'Mobile' },
   { id: 'game', label: 'Games' },
 ]
 
